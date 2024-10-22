107f245571bf306f260bf7be0ab5aed95ee12766
dsosa114 <dasc14@gmail.com>
2024-10-22 16:29:46 -0600

    Now the gripper works as expected, but can be improved

--- custom_ur_control/custom_ur_control/gripper_control.py
+++ custom_ur_control/custom_ur_control/gripper_control.py
@@ -1,6 +1,8 @@
 import rclpy
 import copy 
 
+import numpy as np
+
 from rclpy.node import Node
 from rclpy.action import ActionClient, ActionServer
 from rclpy.action import CancelResponse, GoalResponse
@@ -45,60 +47,153 @@ class GripperPositionControl(Node):
     async def execute_callback(self, goal_handle):
         self.get_logger().info('Executing goal on /robotiq_controller_full...')
         # Relay the goal received from the server to both action clients
-        goal_msg = goal_handle.request
-        command = goal_msg.action
-        self.get_logger().info(f'Received command: {command}')
-        
-        goal_msg.command.position
-        goal_msg_inv = copy.deepcopy(goal_msg.command)
-        goal_msg_inv.position = -1. * goal_msg.command.position
+        goal_received = goal_handle.request
+        # self.get_logger().info(f'Received: {goal_received}')
+        gripper_action = goal_received.action
+        self.get_logger().info(f'Received gripper action: {gripper_action}')
 
-        print(goal_msg_inv)
-        
-        futures = [
-            # Send goals to action clients and wait for their results
-            self._send_goal_to_client(self._action_client_LKJ, goal_msg.command, 'server_LKJ'),
-            self._send_goal_to_client(self._action_client_RKJ, goal_msg_inv, 'server_RKJ'),
-            self._send_goal_to_client(self._action_client_LIKJ, goal_msg.command, 'server_LIKJ'),
-            self._send_goal_to_client(self._action_client_RIKJ, goal_msg_inv, 'server_RIKJ'),
-            self._send_goal_to_client(self._action_client_LFTJ, goal_msg_inv, 'server_LFTJ'),
-            self._send_goal_to_client(self._action_client_RFTJ, goal_msg.command, 'server_RFTJ')
-        ]
-        # Wait for both clients' futures to complete
-        results = await self._wait_for_futures(futures)
-        # result_client_LKJ = await self._send_goal_to_client(self._action_client_LKJ, goal_msg, 'server_LKJ')
-        # result_client_RKJ = await self._send_goal_to_client(self._action_client_RKJ, goal_msg_inv, 'server_RKJ')
-        # result_client_LIKJ = await self._send_goal_to_client(self._action_client_LIKJ, goal_msg, 'server_LIKJ')
-        # result_client_RIKJ = await self._send_goal_to_client(self._action_client_RIKJ, goal_msg_inv, 'server_RIKJ')
-        # result_client_LFTJ = await self._send_goal_to_client(self._action_client_LFTJ, goal_msg_inv, 'server_LFTJ')
-        # result_client_RFTJ = await self._send_goal_to_client(self._action_client_RFTJ, goal_msg, 'server_RFTJ')
-        result_client_LKJ, result_client_RKJ, result_client_LIKJ, result_client_RIKJ, result_client_LFTJ, result_client_RFTJ = results
-        # Process the results and determine the outcome
-        if result_client_LKJ and result_client_RKJ and result_client_LIKJ and result_client_RIKJ and result_client_LFTJ and result_client_RFTJ:
-            self.get_logger().info('All action clients completed successfully.')
+        goal_msg = GripperCommand.Goal()
+        # goal_msg.command = goal_received.command
+        # goal_msg.command.max_effort = goal_received.max_effort
+
+        goal_msg_inv = copy.deepcopy(goal_msg)
+        # goal_msg_inv.command.position = -1. * goal_msg.command.position
+        feedback_msg = GripperPosition.Feedback()
+
+        if np.isclose(goal_received.command.position, self.current_position):
+            self.get_logger().info('The gripper has reached the desirted position.')
             goal_handle.succeed()
 
-            # Return the combined result
-            result = GripperCommand.Result()
-            result.position = result_client_LKJ.result.position
-            result.effort = result_client_LKJ.result.effort
-            result.stalled = result_client_LKJ.result.stalled
-            result.reached_goal = result_client_LKJ.result.reached_goal and result_client_RKJ.result.reached_goal \
-                and result_client_LIKJ.result.reached_goal and result_client_RIKJ.result.reached_goal \
-                and result_client_LFTJ.result.reached_goal and result_client_RFTJ.result.reached_goal
+            result = GripperPosition.Result()
+            result.position = self.current_position
+            result.effort = 0.0
+            result.stalled = False
+            result.reached_goal = True
             return result
-        else:
-            self.get_logger().info('One or multiple action clients failed.')
-            goal_handle.abort()
 
-            result = GripperCommand.Result()
-            result.position = result_client_LKJ.result.position
-            result.effort = result_client_LKJ.result.effort
-            result.stalled = result_client_LKJ.result.stalled
-            result.reached_goal = result_client_LKJ.result.reached_goal and result_client_RKJ.result.reached_goal \
-                and result_client_LIKJ.result.reached_goal and result_client_RIKJ.result.reached_goal \
-                and result_client_LFTJ.result.reached_goal and result_client_RFTJ.result.reached_goal
-            return result
+        elif self.current_position < goal_received.command.position:
+            while self.current_position < goal_received.command.position:
+                step_position = self.current_position + 0.1
+                if step_position >= 0.8:
+                    step_position = 0.8
+                goal_msg.command.position = step_position
+                goal_msg.command.max_effort = goal_received.command.max_effort
+                goal_msg_inv.command.position = -1. * goal_msg.command.position
+
+                futures = [
+                    # Send goals to action clients and wait for their results
+                    self._send_goal_to_client(self._action_client_LKJ, goal_msg, 'server_LKJ'),
+                    self._send_goal_to_client(self._action_client_RKJ, goal_msg_inv, 'server_RKJ'),
+                    self._send_goal_to_client(self._action_client_LIKJ, goal_msg, 'server_LIKJ'),
+                    self._send_goal_to_client(self._action_client_RIKJ, goal_msg_inv, 'server_RIKJ'),
+                    self._send_goal_to_client(self._action_client_LFTJ, goal_msg_inv, 'server_LFTJ'),
+                    self._send_goal_to_client(self._action_client_RFTJ, goal_msg, 'server_RFTJ')
+                ]
+                # Wait for both clients' futures to complete
+                results = await self._wait_for_futures(futures)
+
+                result_client_LKJ, result_client_RKJ, result_client_LIKJ, result_client_RIKJ, result_client_LFTJ, result_client_RFTJ = results
+                # Process the results and determine the outcome
+                if result_client_LKJ and result_client_RKJ and result_client_LIKJ and result_client_RIKJ and result_client_LFTJ and result_client_RFTJ:
+                    feedback_msg.position = step_position
+                    feedback_msg.stalled = False
+
+                    self.joint_state_msg_received = False
+                else:
+                    feedback_msg.position = self.current_position
+                    feedback_msg.stalled = True
+                    break
+
+                goal_handle.publish_feedback(feedback_msg)
+                # self._sleep_async(0.001)
+        else:
+            while self.current_position > goal_received.command.position:
+                step_position = self.current_position - 0.1
+                if step_position <= 0.0:
+                    step_position = 0.0
+                goal_msg.command.position = step_position
+                goal_msg.command.max_effort = goal_received.command.max_effort
+                goal_msg_inv.command.position = -1. * goal_msg.command.position
+
+                futures = [
+                    # Send goals to action clients and wait for their results
+                    self._send_goal_to_client(self._action_client_LKJ, goal_msg, 'server_LKJ'),
+                    self._send_goal_to_client(self._action_client_RKJ, goal_msg_inv, 'server_RKJ'),
+                    self._send_goal_to_client(self._action_client_LIKJ, goal_msg, 'server_LIKJ'),
+                    self._send_goal_to_client(self._action_client_RIKJ, goal_msg_inv, 'server_RIKJ'),
+                    self._send_goal_to_client(self._action_client_LFTJ, goal_msg_inv, 'server_LFTJ'),
+                    self._send_goal_to_client(self._action_client_RFTJ, goal_msg, 'server_RFTJ')
+                ]
+                # Wait for both clients' futures to complete
+                results = await self._wait_for_futures(futures)
+
+                result_client_LKJ, result_client_RKJ, result_client_LIKJ, result_client_RIKJ, result_client_LFTJ, result_client_RFTJ = results
+                # Process the results and determine the outcome
+                if result_client_LKJ and result_client_RKJ and result_client_LIKJ and result_client_RIKJ and result_client_LFTJ and result_client_RFTJ:
+                    feedback_msg.position = step_position
+                    feedback_msg.stalled = False
+
+                    self.joint_state_msg_received = False
+                else:
+                    feedback_msg.position = self.current_position
+                    feedback_msg.stalled = True
+                    break
+
+                goal_handle.publish_feedback(feedback_msg)
+                # self._sleep_async(0.001)
+        
+        goal_handle.succeed()
+        result = GripperPosition.Result()
+        result.position = self.current_position
+        result.effort = 0.0
+        result.stalled = False
+        result.reached_goal = True
+        return result
+        
+        # futures = [
+        #     # Send goals to action clients and wait for their results
+        #     self._send_goal_to_client(self._action_client_LKJ, goal_msg, 'server_LKJ'),
+        #     self._send_goal_to_client(self._action_client_RKJ, goal_msg_inv, 'server_RKJ'),
+        #     self._send_goal_to_client(self._action_client_LIKJ, goal_msg, 'server_LIKJ'),
+        #     self._send_goal_to_client(self._action_client_RIKJ, goal_msg_inv, 'server_RIKJ'),
+        #     self._send_goal_to_client(self._action_client_LFTJ, goal_msg_inv, 'server_LFTJ'),
+        #     self._send_goal_to_client(self._action_client_RFTJ, goal_msg, 'server_RFTJ')
+        # ]
+        # # Wait for both clients' futures to complete
+        # results = await self._wait_for_futures(futures)
+        # # result_client_LKJ = await self._send_goal_to_client(self._action_client_LKJ, goal_msg, 'server_LKJ')
+        # # result_client_RKJ = await self._send_goal_to_client(self._action_client_RKJ, goal_msg_inv, 'server_RKJ')
+        # # result_client_LIKJ = await self._send_goal_to_client(self._action_client_LIKJ, goal_msg, 'server_LIKJ')
+        # # result_client_RIKJ = await self._send_goal_to_client(self._action_client_RIKJ, goal_msg_inv, 'server_RIKJ')
+        # # result_client_LFTJ = await self._send_goal_to_client(self._action_client_LFTJ, goal_msg_inv, 'server_LFTJ')
+        # # result_client_RFTJ = await self._send_goal_to_client(self._action_client_RFTJ, goal_msg, 'server_RFTJ')
+        # result_client_LKJ, result_client_RKJ, result_client_LIKJ, result_client_RIKJ, result_client_LFTJ, result_client_RFTJ = results
+        # # Process the results and determine the outcome
+        # if result_client_LKJ and result_client_RKJ and result_client_LIKJ and result_client_RIKJ and result_client_LFTJ and result_client_RFTJ:
+        #     self.get_logger().info('All action clients completed successfully.')
+        #     goal_handle.succeed()
+
+        #     # Return the combined result
+        #     result = GripperPosition.Result()
+        #     result.position = result_client_LKJ.result.position
+        #     result.effort = result_client_LKJ.result.effort
+        #     result.stalled = result_client_LKJ.result.stalled
+        #     result.reached_goal = result_client_LKJ.result.reached_goal and result_client_RKJ.result.reached_goal \
+        #         and result_client_LIKJ.result.reached_goal and result_client_RIKJ.result.reached_goal \
+        #         and result_client_LFTJ.result.reached_goal and result_client_RFTJ.result.reached_goal
+        #     return result
+        # else:
+        #     self.get_logger().info('One or multiple action clients failed.')
+        #     goal_handle.abort()
+
+        #     result = GripperPosition.Result()
+        #     result.position = result_client_LKJ.result.position
+        #     result.effort = result_client_LKJ.result.effort
+        #     result.stalled = result_client_LKJ.result.stalled
+        #     result.reached_goal = result_client_LKJ.result.reached_goal and result_client_RKJ.result.reached_goal \
+        #         and result_client_LIKJ.result.reached_goal and result_client_RIKJ.result.reached_goal \
+        #         and result_client_LFTJ.result.reached_goal and result_client_RFTJ.result.reached_goal
+        #     return result
 
     def _sleep_async(self, duration):
         # A simple async sleep helper function
@@ -156,7 +251,7 @@ class GripperPositionControl(Node):
             limit_exceeded = [False] * len(msg.name)
             for idx, enum in enumerate(msg.name):
                 if enum == 'robotiq_85_left_knuckle_joint':
-                    self.current_position = msg.position[idx]
+                    self.current_position = np.round(msg.position[idx], 2)
                     self.get_logger().info(f'Current gripper position is: {self.current_position}')
 
             self.joint_state_msg_received = True
