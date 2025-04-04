64bb09eb36241a8c5c1ab189f1685f13a6c0d6c1
dsosa114 <dasc14@gmail.com>
2025-04-04 10:40:42 -0600

    Will test moveit in the big bazu project

--- mark_one_arm_moveit_setup/launch/warehouse_db.launch.py
+++
@@ -1,7 +0,0 @@
-from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_warehouse_db_launch
-
-
-def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("mark_one_arm", package_name="mark_one_arm_moveit_setup").to_moveit_configs()
-    return generate_warehouse_db_launch(moveit_config)
